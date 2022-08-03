343cca4bb8a686d06129064c3e1e1ec41b4c408a
Shimokazu96 <aloe1dental@gmail.com>
2022-08-03 14:18:44 +0900

    material ui導入

--- resources/ts/router.tsx
+++ resources/ts/router.tsx
@@ -1,29 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import TopPage from "./pages/index";
-import TaskPage from "./pages/tasks";
-import LoginPage from "./pages/login";
+import TopPage from "@/components/pages/index";
+import TaskPage from "@/components/pages/tasks";
+import LoginPage from "@/components/pages/login";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/tasks">Tasks</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
                 <Routes>
                     <Route path="/" element={<TopPage />} />
-                    <Route path="/tasks" element={<TaskPage />} />
+                    <Route path="/task" element={<TaskPage />} />
                     <Route path="/login" element={<LoginPage />} />
                 </Routes>
             </div>
