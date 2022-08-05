2ab961cd57fb413464093ce40b3fe81cf09fe15e
Shimokazu96 <aloe1dental@gmail.com>
2022-08-05 12:23:00 +0900

    taskデータの表示

--- resources/ts/components/pages/tasks/index.tsx
+++ resources/ts/components/pages/tasks/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import Dashboard from "@/components/templates/dashboard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { AxiosError, AxiosResponse } from "axios";
+import { Task } from "@/types/Task";
 
 const TaskPage: React.FC = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const getTasks = async () => {
+        await axios
+            .get("api/tasks")
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+                setTasks(response.data);
+            })
+            .catch((err: AxiosError) => console.log(err.response));
+        return tasks;
+    };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
     return (
         <Dashboard title="トップページ">
             <>Task Page</>
+            {tasks.map((task: Task, index) => {
+                return (
+                    <ul key={index}>
+                        <li className="">{task.title}</li>
+                    </ul>
+                );
+            })}
         </Dashboard>
     );
 };
