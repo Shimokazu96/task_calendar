bd408b447e326d6db2bd374fa0ff4b357a1ffce9
shimooka <k.shimooka@1st-net.jp>
2022-08-16 18:35:59 +0900

    Loginページバリエーション設定

--- resources/ts/components/pages/login/index.tsx
+++ resources/ts/components/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { ChangeEvent, useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import {
     Avatar,
@@ -16,8 +16,9 @@ import {
     ThemeProvider,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-
-import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import { AxiosError, AxiosResponse } from "axios";
+import { useForm } from "react-hook-form";
 
 // POSTデータの型
 type LoginForm = {
@@ -34,14 +35,60 @@ type Validation = {
 
 const theme = createTheme();
 
-const Login: React.FC = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
+const Login = () => {
+    // React-Hook-Form
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<LoginForm>();
+
+    // state定義
+    const [validation, setValidation] = useState<Validation>({});
+
+    const onSubmit = async (data: LoginForm) => {
+        setValidation({});
+        // const loginParams: LoginForm = { email, password };
+        // console.log(loginParams);
+        await axios
+            // CSRF保護の初期化
+            .get("/sanctum/csrf-cookie")
+            .then((response: AxiosResponse) => {
+                // ログイン処理
+                axios
+                    .post("api/login", data)
+                    .then((response: AxiosResponse) => {
+                        console.log(response.data);
+                    })
+                    .catch((err: any) => {
+                        console.log(err.response);
+                        // バリデーションエラー
+                        if (err.response?.status === 422) {
+                            const errors = err.response?.data.errors;
+                            // state更新用のオブジェクトを別で定義
+                            const validationMessages: {
+                                [index: string]: string;
+                            } = {} as Validation;
+                            Object.keys(errors).map((key: string) => {
+                                validationMessages[key] = errors[key][0];
+                            });
+                            // state更新用オブジェクトに更新
+                            setValidation(validationMessages);
+                        }
+                        if (err.response?.status === 500) {
+                            alert("システムエラーです！！");
+                        }
+                    });
+            });
+    };
+
+    // SPA認証済みではないとアクセスできないAPI
+    const handleUserClick = () => {
+        axios
+            .get("/api/user", { withCredentials: true })
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+            });
     };
 
     return (
@@ -62,13 +109,18 @@ const Login: React.FC = () => {
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        noValidate
-                        sx={{ mt: 1 }}
-                    >
+                    <Box sx={{ mt: 1 }}>
                         <TextField
+                            {...register("email", {
+                                required: "必須入力です。",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message:
+                                        "有効なメールアドレスを入力してください。",
+                                },
+                            })}
+                            error={"email" in errors}
+                            helperText={errors.email?.message}
                             margin="normal"
                             required
                             fullWidth
@@ -79,6 +131,16 @@ const Login: React.FC = () => {
                             autoFocus
                         />
                         <TextField
+                            {...register("password", {
+                                required: "必須入力です。",
+                                pattern: {
+                                    value: /^([a-zA-Z0-9]{8,})$/,
+                                    message:
+                                        "8文字以上の半角英数字で入力してください",
+                                },
+                            })}
+                            error={"password" in errors}
+                            helperText={errors.password?.message}
                             margin="normal"
                             required
                             fullWidth
@@ -88,13 +150,28 @@ const Login: React.FC = () => {
                             id="password"
                             autoComplete="current-password"
                         />
+                        <Typography
+                            sx={{ color: "error.main" }}
+                            component="div"
+                            variant="body1"
+                        >
+                            {validation.email}
+                        </Typography>
                         <Button
-                            type="submit"
+                            onClick={handleSubmit(onSubmit)}
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
                         >
                             Login
+                        </Button>
+                        <Button
+                            onClick={handleUserClick}
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Login Check
                         </Button>
                         <Grid container>
                             <Grid item xs>
