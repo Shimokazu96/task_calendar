e75a3f1a94aad327e4589bd6176d8b8e57f11f6b
shimooka <k.shimooka@1st-net.jp>
2022-08-19 15:31:33 +0900

    データベース追加

--- database/migrations/2022_08_05_111801_create_sections_table.php
+++ database/migrations/2022_08_05_111801_create_sections_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('sections', function (Blueprint $table) {
             $table->id();
-            $table->string('task_name');
-            $table->boolean('display_flag')->default(false);
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('sections');
     }
 };
---
+++ database/migrations/2022_08_06_111801_create_public_tasks_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('task_id')
+                ->references('id')
+                ->on('tasks')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('section_id');
+            $table->foreign('section_id')
+                ->references('id')
+                ->on('sections')
+                ->onDelete('cascade');
+            $table->integer('required_personnel');
+            $table->integer('determined_personnel')->nullable();
+            $table->text('description');
+            $table->string('date');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->boolean('display_flag')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_tasks');
+    }
+};
