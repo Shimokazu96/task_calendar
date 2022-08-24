711c5b52234f87d6ea35481f2c1840a8e9412b48
shimooka <k.shimooka@1st-net.jp>
2022-08-24 14:22:40 +0900

    fix

--- resources/ts/components/pages/admin/public_task/edit.tsx
+++ resources/ts/components/pages/admin/public_task/edit.tsx
@@ -12,29 +12,41 @@ import {
     Typography,
     Container,
     Paper,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    FormHelperText,
     CircularProgress,
 } from "@mui/material";
+import Select from "@mui/material/Select";
 import { useForm } from "react-hook-form";
-import { Task, Validation, Form } from "@/types/Task";
+import { PublicTask, Validation, Form } from "@/types/PublicTask";
+import { format } from "date-fns";
 import useNotification from "@/hooks/useNotification";
+import CustomDatePicker from "@/components/templates/admin/CustomDatePicker";
+import CustomTimePicker from "@/components/templates/admin/CustomTimePicker";
+
+type Task = {
+    id: number;
+    task_name: string;
+};
+type Section = {
+    id: number;
+    section_name: string;
+};
 
 const EditPublicTaskPage: React.FC = () => {
-    const params = useParams(); // URLのパスパラメータを取得。例えば、 /uses/2 なら、2の部分を取得
     const navigate = useNavigate();
-    const { updated } = useNotification();
-
-    const [validation, setValidation] = useState<Validation>({});
-    const [task, setTask] = useState<Task>({
-        id: 0,
-        task_name: "",
-        description: "",
-        created_at: "",
-        updated_at: "",
-    });
+    const { saved, error } = useNotification();
     const [loading, setLoading] = useState(true);
+
+    const [publicTask, setPublicTask] = useState([]);
+    const [tasks, setTasks] = useState([]);
+    const [sections, setSections] = useState([]);
 
     // React-Hook-Form
     const {
+        control,
         register,
         handleSubmit,
         formState: { errors },
@@ -42,51 +54,71 @@ const EditPublicTaskPage: React.FC = () => {
 
     const getTask = async () => {
         await axiosApi
-            .get(`/api/admin/task/${params.id}`)
+            .get(`/api/admin/task_name`)
             .then((response: AxiosResponse) => {
                 console.log(response.data);
-                setTask(response.data);
+                setTasks(response.data);
                 setLoading(false);
             })
             .catch((err: AxiosError) => console.log(err.response));
-        return task;
+        return tasks;
+    };
+    const getSection = async () => {
+        await axiosApi
+            .get(`/api/admin/section_name`)
+            .then((response: AxiosResponse) => {
+                console.log(response.data);
+                setSections(response.data);
+                setLoading(false);
+            })
+            .catch((err: AxiosError) => console.log(err.response));
+        return sections;
     };
 
     useEffect(() => {
         getTask();
+        getSection();
     }, []);
-    console.log(task);
 
     const onSubmit = async (data: Form) => {
-        setValidation({});
+        let formData = new FormData();
+        const config = {
+            headers: { "content-type": "multipart/form-data" },
+        };
+        formData.append("task_id", data.task_id);
+        formData.append("section_id", data.section_id);
+        formData.append("required_personnel", data.required_personnel);
+        data.description
+            ? formData.append("description", data.description)
+            : formData.append("description", "");
+        formData.append("date", format(data.date, "yyyy-MM-dd"));
+        formData.append("start_time", format(data.start_time, "HH:mm"));
+        formData.append("end_time", format(data.end_time, "HH:mm"));
+        for (let value of formData.entries()) {
+            console.log(value);
+        }
         await axiosApi
-            .put(`/api/admin/task/${params.id}`, data)
+            .post(`/api/admin/public_task`, formData, config)
             .then((response: AxiosResponse) => {
                 console.log(response.data);
-                updated();
-                navigate(`/admin/task/${params.id}`);
+                saved();
+                navigate(`/admin/public_task`);
             })
             .catch((err: any) => {
                 console.log(err.response);
                 // バリデーションエラー
                 if (err.response?.status === 422) {
                     const errors = err.response?.data.errors;
-                    // state更新用のオブジェクトを別で定義
-                    const validationMessages: {
-                        [index: string]: string;
-                    } = {} as Validation;
+
                     Object.keys(errors).map((key: string) => {
-                        validationMessages[key] = errors[key][0];
+                        error(errors[key][0]);
                     });
-                    // state更新用オブジェクトに更新
-                    setValidation(validationMessages);
                 }
                 if (err.response?.status === 500) {
-                    alert("システムエラーです！！");
+                    error("システムエラーです！");
                 }
             });
     };
-
     if (loading) {
         return (
             <Dashboard title="">
@@ -112,7 +144,7 @@ const EditPublicTaskPage: React.FC = () => {
                         component="h2"
                         variant="h5"
                     >
-                        タスク・マスター編集
+                        公開タスク登録
                     </Typography>
                 </Box>
             </Box>
@@ -123,32 +155,158 @@ const EditPublicTaskPage: React.FC = () => {
                 >
                     <Grid container spacing={3}>
                         <Grid item xs={12} md={12}>
-                            <TextField
-                                {...register("task_name", {
-                                    required: "必須入力です。",
-                                })}
-                                error={"task_name" in errors}
-                                sx={{ mb: 2 }}
-                                helperText={errors.task_name?.message}
-                                defaultValue={task.task_name}
-                                required
-                                id="task_name"
-                                label="タスク名"
-                                fullWidth
+                            <Grid
+                                container
+                                sx={{
+                                    display: "flex",
+                                    mb: 2,
+                                }}
+                                alignItems="center"
+                            >
+                                <CustomDatePicker
+                                    valueName="date"
+                                    control={control}
+                                    required
+                                    mr={2}
+                                    label="日付"
+                                />
+                                <CustomTimePicker
+                                    valueName="start_time"
+                                    control={control}
+                                    required
+                                    mr={2}
+                                    label="開始時間"
+                                />
+                                <CustomTimePicker
+                                    valueName="end_time"
+                                    control={control}
+                                    required
+                                    mr={2}
+                                    label="終了時間"
+                                />
+                            </Grid>
+
+                            <FormControl
                                 variant="standard"
-                            ></TextField>
+                                sx={{ mb: 2, mr: 2, minWidth: 300 }}
+                            >
+                                <InputLabel
+                                    sx={
+                                        errors.task_id?.message
+                                            ? { color: "error.main" }
+                                            : { color: "text.secondary" }
+                                    }
+                                    id="task_id"
+                                >
+                                    タスク選択
+                                </InputLabel>
+                                <Select
+                                    defaultValue=""
+                                    {...register("task_id", {
+                                        required: "必須入力です。",
+                                    })}
+                                    error={"task_id" in errors}
+                                    labelId="task_id"
+                                    id="task_id"
+                                    label="task_id"
+                                >
+                                    {tasks?.map((task: Task, index: number) => (
+                                        <MenuItem key={index} value={task.id}>
+                                            {task.task_name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText sx={{ color: "error.main" }}>
+                                    {errors.task_id?.message}
+                                </FormHelperText>
+                            </FormControl>
+                            <FormControl
+                                variant="standard"
+                                sx={{ mb: 2, minWidth: 300 }}
+                            >
+                                <InputLabel
+                                    sx={
+                                        errors.section_id?.message
+                                            ? { color: "error.main" }
+                                            : { color: "text.secondary" }
+                                    }
+                                    id="section_id"
+                                >
+                                    セクション選択
+                                </InputLabel>
+                                <Select
+                                    defaultValue=""
+                                    {...register("section_id", {
+                                        required: "必須入力です。",
+                                    })}
+                                    error={"section_id" in errors}
+                                    labelId="section_id"
+                                    id="section_id"
+                                    label="section_id"
+                                >
+                                    {sections?.map(
+                                        (section: Section, index: number) => (
+                                            <MenuItem
+                                                key={index}
+                                                value={section.id}
+                                            >
+                                                {section.section_name}
+                                            </MenuItem>
+                                        )
+                                    )}
+                                </Select>
+                                <FormHelperText sx={{ color: "error.main" }}>
+                                    {errors.section_id?.message}
+                                </FormHelperText>
+                            </FormControl>
+                            <Grid item xs={12} md={12}>
+                                <TextField
+                                    {...register("required_personnel", {
+                                        required: "必須入力です。",
+                                    })}
+                                    error={"required_personnel" in errors}
+                                    helperText={
+                                        errors.required_personnel?.message
+                                    }
+                                    required
+                                    id="required_personnel"
+                                    label="必要人数"
+                                    type="number"
+                                    sx={{ mb: 2 }}
+                                    InputProps={{ inputProps: { min: 1 } }}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="standard"
+                                />
+                            </Grid>
+                            {/* <Grid item xs={12} md={12}>
+                                <TextField
+                                    {...register("determined_personnel", {
+                                        required: "必須入力です。",
+                                    })}
+                                    error={"determined_personnel" in errors}
+                                    helperText={
+                                        errors.determined_personnel?.message
+                                    }
+                                    id="determined_personnel"
+                                    label="確定人数"
+                                    type="number"
+                                    sx={{ mb: 2 }}
+                                    InputProps={{ inputProps: { min: 1 } }}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="standard"
+                                    required
+                                />
+                            </Grid> */}
                             <TextField
-                                {...register("description", {
-                                    required: "必須入力です。",
-                                })}
-                                error={"description" in errors}
-                                helperText={errors.description?.message}
-                                defaultValue={task.description}
+                                {...register("description")}
                                 multiline
                                 rows={4}
-                                required
                                 id="description"
-                                label="タスク概要"
+                                label="補足"
                                 fullWidth
                                 variant="standard"
                             ></TextField>
@@ -161,7 +319,7 @@ const EditPublicTaskPage: React.FC = () => {
                             style={{ padding: 16 }}
                         >
                             <Grid>
-                                <Link to="/admin/task">
+                                <Link to="/admin/public_task">
                                     <Button>キャンセル</Button>
                                 </Link>
 
