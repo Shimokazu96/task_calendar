d74919c26bfcdc5b0d493cd4156cf82d5ffb30c1
shimooka <k.shimooka@1st-net.jp>
2022-09-02 13:31:12 +0900

    タスク詳細のUIを作成

--- resources/ts/route/index.tsx
+++ resources/ts/route/index.tsx
@@ -4,6 +4,7 @@ import Layout from "@/components/layout/front/Layout";
 const TopPage = lazy(() => import("@/components/pages/index"));
 const CurrentDatePage = lazy(() => import("@/components/pages/date/CurrentDate"));
 const PublicTaskPage = lazy(() => import("@/components/pages/publicTask"));
+const DetailPublicTaskPage = lazy(() => import("@/components/pages/publicTask/detail"));
 const UserPage = lazy(() => import("@/components/pages/user"));
 
 export const MasterRoutes = (
@@ -12,7 +13,8 @@ export const MasterRoutes = (
             <Route index element={<TopPage />} />
             <Route path="/date/:date" element={<CurrentDatePage />} />
             <Route path="/public_task" element={<PublicTaskPage />} />
-            <Route path="/user" element={<UserPage />} />
+            <Route path="/public_task/:id" element={<DetailPublicTaskPage />} />
+            <Route path="/mypage" element={<UserPage />} />
         </Route>
     </>
 );
