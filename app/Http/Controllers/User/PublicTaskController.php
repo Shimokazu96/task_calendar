8b4bcb007c13c7862a1312561ab1205b937ebd22
Shimokazu96 <aloe1dental@gmail.com>
2022-09-12 12:40:49 +0900

    フロント色分け

--- app/Http/Controllers/User/PublicTaskController.php
+++ app/Http/Controllers/User/PublicTaskController.php
@@ -31,6 +31,7 @@ class PublicTaskController extends Controller
                 'start' => $value->date . 'T' . $value->start_time,
                 'end' => $value->date  . 'T' . $value->end_time,
                 'url' => config("app.url") . "/public_task/" . $value->id,
+                'color' => PublicTask::color($value->task->color),
             ];
         }
         return response()->json($public_task, 200) ?? abort(404);
