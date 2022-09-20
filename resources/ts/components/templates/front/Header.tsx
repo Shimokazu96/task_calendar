65003096325bddba1f0b3cac83110dc4d168c10a
Shimokazu96 <aloe1dental@gmail.com>
2022-09-20 13:58:12 +0900

    フロント画面のヘッダーを編集

--- resources/ts/components/templates/front/Header.tsx
+++ resources/ts/components/templates/front/Header.tsx
@@ -1,15 +1,32 @@
 import * as React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
-
+import { Link } from "react-router-dom";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 type Props = {
     title: string;
+    link: string;
 };
 
 export default function Header(props: Props) {
     return (
         <AppBar position="fixed">
-            <Toolbar sx={{ minHeight: "48px", height: "48px" }}>
-                <Typography sx={{ flexGrow: 1 }}>{props.title}</Typography>
+            <Toolbar
+                sx={{
+                    justifyContent: "space-between",
+                    minHeight: "48px",
+                    height: "48px",
+                }}
+            >
+                {props.link ? (
+                    <Link to={props.link}>
+                        <ArrowBackIosNewIcon />
+                    </Link>
+                ) : (
+                    <Typography sx={{ flexGrow: 0 }}>　</Typography>
+                )}
+
+                <Typography sx={{ flexGrow: 0 }}>{props.title}</Typography>
+                <Typography sx={{ flexGrow: 0 }}>　</Typography>
             </Toolbar>
         </AppBar>
     );
