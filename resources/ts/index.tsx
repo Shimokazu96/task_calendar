91aaa002c9b03f301641a032e48aaa5cf9d317b5
Shimokazu96 <aloe1dental@gmail.com>
2022-10-03 12:23:03 +0900

    不要なファイル削除

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -12,8 +12,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, Zoom } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import App from "./App";
 
 ReactDOM.render(
